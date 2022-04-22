7cfad5e834fb8ff874892eaf601e42ff7d9cb4f9
Rafael <rafael_basso81@hotmail.com>
2022-04-22 11:16:07 -0300

    add files

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import routes from './routes'
 
-console.log('server running')
-
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
 
-app.listen(3331)
+app.listen(3331, () => {
+  console.log('Server running on port 3331')
+})
