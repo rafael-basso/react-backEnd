f8ac4db4c178fd23ae4f466c41ca2b6c193a7b4f
Rafael Donizete Basso <rafael.basso@onlineapp.com.br>
2022-12-31 11:21:26 -0300

    ajuste express.json - arquivo server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import express from 'express'
+import express, { RequestHandler, response } from 'express'
 import cors from 'cors'
 import routes from './routes'
+import { request } from 'http';
+import path from 'path/posix';
 
-console.log('server running')
+console.log('server running');
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+const app = express();
+app.use(cors());
+app.use(express.json() as RequestHandler);
+app.use(routes);
 
 
-app.listen(3331)
+app.listen(3331);
