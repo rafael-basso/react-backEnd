5379c0e03a6589720606b0277e654ba87506098c
rafael-basso <rafael_basso81@hotmail.com>
2024-09-24 14:12:30 -0300

    Ajustes - conexão com banco

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
-import express from 'express'
+import express, { RequestHandler, response } from 'express'
 import cors from 'cors'
 import routes from './routes'
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(routes)
-
+const app = express();
+app.use(cors());
+app.use(express.json() as RequestHandler);
+app.use(routes);
 
 app.listen(3331, () => {
   console.log('Server running on port 3331')
