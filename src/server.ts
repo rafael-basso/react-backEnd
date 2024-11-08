e845af76bb6288e54232e347dfc66cab4e2b34e6
rafael-basso <rafael_basso81@hotmail.com>
2024-11-07 22:52:41 -0300

    vercel file included

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,11 @@ import cors from 'cors'
 import routes from './routes'
 
 const app = express();
+const port = process.env.PORT || 3331;
 app.use(cors());
 app.use(express.json() as RequestHandler);
 app.use(routes);
 
-app.listen(3331, () => {
-  console.log('Server running on port 3331')
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
 })
