1616b2fe4ee31a6af31316c9b30d5d353be45069
rafael-basso <rafael_basso81@hotmail.com>
2024-11-08 00:10:31 -0300

    ajuste link front

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ console.log('server running');
 const app = express();
 // app.use(cors());
 app.use(cors({
-  origin: ['https://react-front-kdmriwteu-rafael-bassos-projects.vercel.app/', 'http://localhost:3000'],
+  origin: ['https://react-front-end-opal.vercel.app/', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: false
