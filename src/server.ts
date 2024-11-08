465c5a0ebb58a896dc62276cd1f7b17b0137b60e
rafael-basso <rafael_basso81@hotmail.com>
2024-11-07 23:19:04 -0300

    inserir url front-end e config no package.json

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,12 @@ import routes from './routes'
 console.log('server running');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: ['https://react-front-kdmriwteu-rafael-bassos-projects.vercel.app/', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json() as RequestHandler);
 app.use(routes);
-
 
 app.listen(3331);
