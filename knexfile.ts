e191cb97b44040ab1d91aafe883d103c7acc4e6e
rafael-basso <rafael_basso81@hotmail.com>
2024-11-07 23:53:23 -0300

    add vercel config to run build

--- knexfile.ts
+++ knexfile.ts
@@ -1,15 +1,24 @@
 import path from 'path'
 
 module.exports = {
-    client: 'sqlite3',
-    connection: {
-        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+    development: {
+        client: 'sqlite3',
+        connection: {
+            filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+        },
+        migrations: {
+            directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+        },
+        // seeds: {
+        //     directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+        // },
+        useNullAsDefault: true,
     },
-    migrations: {
-        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-    },
-    // seeds: {
-    //     directory: path.resolve(__dirname, 'src', 'database', 'seeds')
-    // },
-    useNullAsDefault: true,
+    production: {
+        client: 'sqlite3',
+        connection: {
+          filename: path.join('/tmp', 'database.sqlite')
+        },
+        useNullAsDefault: true
+      }
 }
