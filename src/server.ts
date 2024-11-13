5598d8cc7432529a2e82f41bef65a34da225681c
rafael-basso <rafael_basso81@hotmail.com>
2024-11-13 13:44:35 -0300

    permitir cross-origin

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,13 @@ import routes from './routes'
 console.log('server running');
 
 const app = express();
-// app.use(cors());
-app.use(cors({
-  origin: ['https://react-front-end-opal.vercel.app/', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: ['https://react-front-end-opal.vercel.app/', 'http://localhost:3000'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// }));
 app.use(express.json() as RequestHandler);
 app.use(routes);
 
